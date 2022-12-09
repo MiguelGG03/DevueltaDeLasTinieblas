9ccc7ea9222ace9d48a441d9f8896832dee02659
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-09 16:14:49 +0000

    ordered dict implementado completamente

--- orderdict.py
+++ orderdict.py
@@ -6,6 +6,22 @@ n['dos'] = 'two'
 n['tres'] = 'three'
 
 print(n)
+print('Sin OrderedDict:')
+
+n1 = {}
+n1['uno'] = 'one'
+n1['dos'] = 'two'
+
+n2 = {}
+n2['dos'] = 'two'
+n2['uno'] = 'one'
+
+# ¿Tienen los mismos elementos y en el mismo orden?
+print(n1)
+print(n2)
+print(n1 == n2)
+
+print('Con OrderedDict:')
 
 n1 = OrderedDict()
 n1['uno'] = 'one'
@@ -16,5 +32,6 @@ n2['dos'] = 'two'
 n2['uno'] = 'one'
 
 # ¿Tienen los mismos elementos y en el mismo orden?
+print(dict(n1))
+print(dict(n2))
 print(n1 == n2)
-False
