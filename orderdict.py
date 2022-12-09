f92943748f61426a26bdd153cf3be8dc5b1b9459
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-09 16:11:35 +0000

    orderdict para mantengan el orden del dict

--- orderdict.py
+++ orderdict.py
@@ -6,3 +6,15 @@ n['dos'] = 'two'
 n['tres'] = 'three'
 
 print(n)
+
+n1 = OrderedDict()
+n1['uno'] = 'one'
+n1['dos'] = 'two'
+
+n2 = OrderedDict()
+n2['dos'] = 'two'
+n2['uno'] = 'one'
+
+# ¿Tienen los mismos elementos y en el mismo orden?
+print(n1 == n2)
+False
